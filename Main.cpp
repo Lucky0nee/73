934935f1104a873682faf13b83973ff253578eb4
Lucky0nee <65708257+Lucky0nee@users.noreply.github.com>
2023-03-14 20:10:20 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -19,7 +19,7 @@ private:
 	int m_denominator = 1;
 };
 
-std::unique_ptr<Fraction> DoSomething(int firstNum, int secondNum) {
+std::shared_ptr<Fraction> DoSomething(int firstNum, int secondNum) {
 	for (int i = 2; i < 17; i++) {
 		if (firstNum % i == 0 && secondNum % i == 0) {
 			firstNum /= i; //std::cout << firstNum << " ";
@@ -28,7 +28,7 @@ std::unique_ptr<Fraction> DoSomething(int firstNum, int secondNum) {
 		}
 		//std::cout << i;
 	}
-	return std::make_unique<Fraction>(firstNum, secondNum);
+	return std::make_shared<Fraction>(firstNum, secondNum);
 }
 
 void printFraction(std::shared_ptr<Fraction> ptr) { // const Fraction* const ptr
@@ -36,7 +36,7 @@ void printFraction(std::shared_ptr<Fraction> ptr) { // const Fraction* const ptr
 }
 
 int main() {
-	std::shared_ptr<Fraction> ptr = std::make_unique<Fraction>(49, 11711);
+	std::shared_ptr<Fraction> ptr = std::make_shared<Fraction>(49, 11711);
 
 	std::cout << "\n" << "Початковий дріб: ";
 	printFraction(std::move(ptr));
